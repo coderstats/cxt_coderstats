05aa10273f77e7b74dd652985b3d8eea3490e52e
Ramiro Gomez <web@ramiro.org>
2012-02-27 01:09:21 +0100

    first commit

---
+++ coderstats/coderstats.js
@@ -0,0 +1,2 @@
+var url='http://coderstats.geeksta.net/coder/'+$('span[itemprop="nickname"]').text();$('.page-profile .vcard').append('<dl><dt>Coderstats</dt><dd><a href="'+url+'">'+url+'</a></dd></dl>');
+
