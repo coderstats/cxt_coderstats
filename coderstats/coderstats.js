b72b97fda4c16c60e0074f29c0a781353dc7389b
Ramiro Gomez <web@ramiro.org>
2012-03-06 22:04:56 +0100

    make ext work on both user and organization pages which have different markup on Github

--- coderstats/coderstats.js
+++ coderstats/coderstats.js
@@ -1,2 +1,4 @@
-var url='http://coderstats.geeksta.net/coder/'+$('span[itemprop="nickname"]').text();$('.page-profile .vcard').append('<dl><dt>Coderstats</dt><dd><a href="'+url+'">'+url+'</a></dd></dl>');
+var nick = $('span[itemprop]="nickname"').text();// assume user page
+if (!nick) nick = $('.pagehead.userpage').attr('data-name');// org page
+var url='http://coderstats.geeksta.net/coder/'+$.trim(nick);$('.page-profile .vcard').append('<dl><dt>Coderstats</dt><dd><a href="'+url+'">'+url+'</a></dd></dl>');
 
