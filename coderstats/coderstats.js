bbd723f4d3b43147293b1dff116ae578de1490ee
Ramiro Gomez <web@ramiro.org>
2012-03-10 01:54:14 +0100

    user page change

--- coderstats/coderstats.js
+++ coderstats/coderstats.js
@@ -1,4 +1,4 @@
-var nick = $('span[itemprop]="nickname"').text();// assume user page
+var nick = $('.columns.profilecols"').attr('data-name');// assume user page
 if (!nick) nick = $('.pagehead.userpage').attr('data-name');// org page
 var url='http://coderstats.geeksta.net/coder/'+$.trim(nick);$('.page-profile .vcard').append('<dl><dt>Coderstats</dt><dd><a href="'+url+'">'+url+'</a></dd></dl>');
 
