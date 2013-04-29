72af4bbbbe26d9c61495a877ffb2b283f62c54c2
Ramiro Gómez <web@ramiro.org>
2013-04-29 16:41:43 +0200

    fix #1: determine user name from url

--- coderstats/coderstats.js
+++ coderstats/coderstats.js
@@ -1,3 +1,5 @@
-var nick = $('em[itemprop="additionalName"]').text();// assume user page
-var url='http://coderstats.geeksta.net/coder/'+$.trim(nick);$('.details').append('<dl><dt><span class="mini-icon mini-icon-link"></span></dt><dd><a href="'+url+'">Coderstats</a></dd></dl>');
-
+var path = document.location.pathname;
+if (m = path.match(/^\/([\w-]+)\??.*?/)) {
+    var nick = m[1];
+    var url='http://cs.geeksta.net/github/'+$.trim(nick);$('.details').append('<dl><dt><span class="mini-icon mini-icon-link"></span></dt><dd><a href="'+url+'">Coderstats</a></dd></dl>');
+}
