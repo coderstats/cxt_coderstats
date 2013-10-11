18fb4ff4860154052c9857789a08275445af5255
Ramiro Gómez <code@ramiro.org>
2013-10-12 00:35:09 +0200

    adjusted to github dom changes

--- coderstats/coderstats.js
+++ coderstats/coderstats.js
@@ -3,11 +3,13 @@ if (m = path.match(/^\/([\w-]+)\??.*?/)) {
     var login = m[1];
     if (-1 === ['timeline', 'languages', 'blog', 'explore'].indexOf(login)) {
         var url = 'http://coderstats.net/github/' + login.trim() + '/';
-        var details = document.getElementsByClassName('details');
+        var details = document.getElementsByClassName('vcard-details');
         if (details.length > 0) {
-            var dl = document.createElement('dl');
-            dl.innerHTML = '<dt><span class="octicon octicon-link"></span></dt><dd><a href="' + url + '">CoderStats(\'' + login + '\')</a></dd>';
-            details[0].appendChild(dl);
+            var li = document.createElement('li');
+            li.setAttribute('class', 'vcard-detail');
+            li.setAttribute('itemprop', 'url');
+            li.innerHTML = '<span class="octicon octicon-link"></span><a href="' + url + '">CoderStats(\'' + login + '\')</a>';
+            details[0].appendChild(li);
         }
     }
 }
